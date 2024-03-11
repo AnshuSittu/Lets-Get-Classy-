8d86acfa45c37c3d3e9d2d7da778b7885e06fdc7
Anshu <anshu.possible@gmail.com>
2024-03-11 16:39:35 +0530

    destructuring in classed based components

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,17 +4,20 @@ class UserClass extends React.Component {
   constructor(props) {
     super(props);
 
-    console.log(props);
+  //  console.log(props);
   }
 
   render() {
+    //destructuring in class components
+    const {name, location} = this.props;
+
     return (
       <div className="user-cart">
         <h1 style={{ textAlign: "Center" }}>
           This is coming from Class based Component{" "}
         </h1>
-        <h2>Name: {this.props.name}</h2>
-        <h3>Location: {this.props.location}</h3>
+        <h2>Name: {name}</h2>
+        <h3>Location: {location}</h3>
         <h3>Contact: anshu.possible@gmail.com</h3>
       </div>
     );
