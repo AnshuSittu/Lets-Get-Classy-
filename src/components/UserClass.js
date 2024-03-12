5b5b335b496a2c8897bc3f7e0b2a64b99b66a8d3
Anshu <anshu.possible@gmail.com>
2024-03-12 11:50:16 +0530

    First constructor is callled then Render is called

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -8,22 +8,25 @@ class UserClass extends React.Component {
       count: 0,
       count2: 1,
     };
+    console.log("constructor")
   }
 
   render() {
     const { name, location } = this.props;
     const { count } = this.state;
 
+   console.log("Render")
+
     return (
       <div className="user-cart">
         <h1 style={{ textAlign: "Center" }}>
-          This is coming from Class based Component{" "}
+          This is coming from Class based Component
         </h1>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h3>Contact: anshu.possible@gmail.com</h3>
         <h4>
-          Count = {count}{" "}
+          Count = {count}
           <button
             onClick={() => {
               this.setState({
@@ -34,7 +37,7 @@ class UserClass extends React.Component {
             }}
           >
             Click Me
-          </button>{" "}
+          </button>
         </h4>
       </div>
     );
